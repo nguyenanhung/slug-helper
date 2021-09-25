824060a7a912ae05e69c45abe417b10122cf26f8
nguyenanhung <info@nguyenanhung.com>
2021-09-25 08:50:53 +0700

    Update Test Case

--- test/test.php
+++ test/test.php
@@ -10,4 +10,5 @@ $name = 'Nguyễn An Hưng';
 
 echo "slugify: " . $slugUrl->slugify($name) . PHP_EOL;
 echo "searchSlugify: " . $slugUrl->searchSlugify($name) . PHP_EOL;
-echo "strToEn: " . $slugUrl->strToEn($name) . PHP_EOL;
+echo "toEnglish: " . $slugUrl->toEnglish($name) . PHP_EOL;
+echo "convertVietnameseToEnglish: " . $slugUrl->convertVietnameseToEnglish($name) . PHP_EOL;
